aa7043a39aec45179ac461c83fbcb8fb7dfebd3d
Ferhat Suat Erdoğan <30902468+ekofi@users.noreply.github.com>
2023-05-28 16:17:15 +0300

    update

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -29,11 +29,11 @@ var cbpAnimatedHeader = (function () {
 
   function scrollPage() {
     var sy = scrollY();
-    if (sy >= changeHeaderOn) {
+    /* if (sy >= changeHeaderOn) {
       classie.add(header, "navbar-shrink");
     } else {
       classie.remove(header, "navbar-shrink");
-    }
+    } */
     didScroll = false;
   }
 
